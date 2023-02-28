ce6e68d1e0883b82d4e64256e50e83068d2b8a23
html.jackson <html.jackson@gmail.com>
2023-02-28 05:58:50 +0300

    slider fix

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,40 +1,31 @@
-// const tabletMedia = 1250;
+const tabletMedia = 1250;
+let swiper = Swiper;
+let init = false;
 
-// let init = false;
+function swiperCard() {
+  if (window.innerWidth <= tabletMedia) {
+    if (!init) {
+      init = true;
+      swiper = new Swiper(".swiper", {
+        slidesPerView: 1,
+        navigation: {
+          nextEl: ".swiper .swiper-button-next",
+          prevEl: ".swiper .swiper-button-prev",
+        },
+      });
+    }
+  } else if (init) {
+    swiper.destroy();
+    init = false;
+  }
+}
+swiperCard();
+window.addEventListener("resize", swiperCard);
 
-// function swiperCard() {
-//   if (window.innerWidth <= tabletMedia) {
-//     if (!init) {
-//       init = true;
-//       const swiper = new Swiper(".swiper-1", {
-//         slidesPerView: 1,
-//         navigation: {
-//           nextEl: '.swiper-1 .swiper-button-next',
-//           prevEl: '.swiper-1 .swiper-button-prev',
-//         },
-//       });
-//     }
-//   } else if (init) {
-//     swiper.destroy();
-//     init = false;
-//   }
-// }
-// swiperCard();
-// window.addEventListener("resize", swiperCard);
+const toggle = document.querySelector(".header__toggle");
+const nav = document.querySelector(".nav");
 
-const swiper = new Swiper(".swiper", {
-  slidesPerView: 1,
-  navigation: {
-    nextEl: '.swiper .swiper-button-next',
-    prevEl: '.swiper .swiper-button-prev',
-  },
-});
-
-
-const toggle = document.querySelector('.header__toggle');
-const nav = document.querySelector('.nav');
-
-toggle.addEventListener('click', () => {
-  console.log('clicked');
-  nav.classList.toggle('hidden');
+toggle.addEventListener("click", () => {
+  console.log("clicked");
+  nav.classList.toggle("hidden");
 });
